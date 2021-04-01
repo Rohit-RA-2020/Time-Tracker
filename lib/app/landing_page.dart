e1a59856300d156c3227944ba48efb7bca6c6d67
Rohit ranjan <ranjanrohit812@gmail.com>
2021-04-01 22:05:22 +0530

    Adding user to landing page

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:time_tracker_flutter/app/sign_in/sign_in_page.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  User _user;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_user == null) {
+      return SignInPage();
+    }
+    return Container();
+  }
+}
