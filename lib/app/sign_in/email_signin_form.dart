1a0b50879b8591975f6e190216c2335f4afb0442
Rohit ranjan <ranjanrohit812@gmail.com>
2021-04-04 21:57:50 +0530

    Toggling b/w SignIn and Register

--- lib/app/sign_in/email_signin_form.dart
+++ lib/app/sign_in/email_signin_form.dart
@@ -1,21 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter/common_widgets/form_submit_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
+
+class EmailSignInForm extends StatefulWidget {
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
+  void _submit() {
+    print('email: ${_emailController.text}');
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? 'Sign In'
+        : 'Create an account';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign In';
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: 'Email',
           hintText: 'Enter your mail ID',
         ),
+        keyboardType: TextInputType.emailAddress,
       ),
+      SizedBox(height: 8.0),
       TextField(
+        controller: _passwordController,
         decoration: InputDecoration(
           labelText: 'Password',
           hintText: 'Enter your password',
         ),
         obscureText: true,
       ),
+      SizedBox(height: 16.0),
+      FormSubmitButton(
+        onPressed: _submit,
+        text: primaryText,
+      ),
+      SizedBox(height: 8.0),
+      TextButton(
+        child: Text(secondaryText),
+        onPressed: _toggleFormType,
+      )
     ];
   }
 
@@ -24,6 +72,7 @@ class EmailSignInForm extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: _buildChildren(),
       ),
