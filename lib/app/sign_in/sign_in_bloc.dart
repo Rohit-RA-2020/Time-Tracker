d02423cabd718bc34b80c13c72c9528a7987e50a
Rohit ranjan <ranjanrohit812@gmail.com>
2021-04-09 13:10:53 +0530

    SignIn BloC

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -1,12 +1,38 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter/services/auth.dart';
 
 class SignInBloc {
+  SignInBloc({@required this.auth});
+  final AuthBase auth;
+
   final StreamController<bool> _isLoadingController = StreamController<bool>();
+
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
   void dispose() {
     _isLoadingController.close();
   }
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
+  }
+
+  Future<User> signInAnonymously() async =>
+      await _signIn(auth.signInAnonymously);
+
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+
+  Future<User> signInWithFacebook() async =>
+      await _signIn(auth.signInWithFacebook);
 }
