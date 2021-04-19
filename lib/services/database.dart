88723c6b458bc5a94811de6834964ddf3a5f3e0e
Rohit ranjan <ranjanrohit812@gmail.com>
2021-04-19 15:38:38 +0530

    Added Provider with database

---
+++ lib/services/database.dart
@@ -0,0 +1,9 @@
+import 'package:meta/meta.dart';
+
+abstract class Database {}
+
+class FirestoreDatabase implements Database {
+  FirestoreDatabase({@required this.uid}) : assert(uid != null);
+
+  final String uid;
+}
