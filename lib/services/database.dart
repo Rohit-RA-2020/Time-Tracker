c0d09e193bcffd210ef73c53ef0d5bb15b43221f
Rohit ranjan <ranjanrohit812@gmail.com>
2021-04-23 21:53:21 +0530

    Adding factory constructor

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -22,17 +22,11 @@ class FirestoreDatabase implements Database {
     final path = APIPath.jobs(uid);
     final reference = FirebaseFirestore.instance.collection(path);
     final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs.map(
-          (snapshot) {
-            final data = snapshot.data();
-            return data != null
-                ? Job(
-                    name: data['name'],
-                    ratePerHour: data['ratePerHour'],
-                  )
-                : null;
-          },
-        ).toList());
+    return snapshots.map((snapshot) => snapshot.docs
+        .map(
+          (snapshot) => Job.fromMap(snapshot.data()),
+        )
+        .toList());
   }
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
