e21c82f1ba80ba319e37adf98a971ff09f8cd434
ROHIT-RA-2020 <ranjanrohit812@gmail.com>
2021-04-30 22:02:02 +0530

    Improving List View using ListTile

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,10 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter/app/home/jobs/add_job_page.dart';
+import 'package:time_tracker_flutter/app/home/jobs/job_list_file.dart';
 import 'package:time_tracker_flutter/app/home/models/job.dart';
 import 'package:time_tracker_flutter/common_widgets/show_alert_dialog.dart';
-import 'package:time_tracker_flutter/common_widgets/show_exception_alert_dialog.dart';
 import 'package:time_tracker_flutter/services/auth.dart';
 import 'package:time_tracker_flutter/services/database.dart';
 
@@ -64,7 +63,14 @@ class JobsPage extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final jobs = snapshot.data;
-          final children = jobs.map((job) => Text(job.name)).toList();
+          final children = jobs
+              .map(
+                (job) => JobListTile(
+                  job: job,
+                  onTap: () {},
+                ),
+              )
+              .toList();
           return ListView(children: children);
         }
         if (snapshot.hasError) {
