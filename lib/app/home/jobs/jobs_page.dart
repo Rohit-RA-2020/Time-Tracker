d1b45b530743532dcd0e7a5058ccdbc9d19959f0
ROHIT-RA-2020 <ranjanrohit812@gmail.com>
2021-05-01 17:37:35 +0530

    Page for Empty content

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter/app/home/jobs/edit_job_page.dart';
+import 'package:time_tracker_flutter/app/home/jobs/empty_content.dart';
 import 'package:time_tracker_flutter/app/home/jobs/job_list_file.dart';
 import 'package:time_tracker_flutter/app/home/models/job.dart';
 import 'package:time_tracker_flutter/common_widgets/show_alert_dialog.dart';
@@ -63,15 +64,18 @@ class JobsPage extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final jobs = snapshot.data;
-          final children = jobs
-              .map(
-                (job) => JobListTile(
-                  job: job,
-                  onTap: () => EditJobPage.show(context, job: job),
-                ),
-              )
-              .toList();
-          return ListView(children: children);
+          if (jobs.isNotEmpty) {
+            final children = jobs
+                .map(
+                  (job) => JobListTile(
+                    job: job,
+                    onTap: () => EditJobPage.show(context, job: job),
+                  ),
+                )
+                .toList();
+            return ListView(children: children);
+          }
+          return EmptyContent();
         }
         if (snapshot.hasError) {
           return Center(
