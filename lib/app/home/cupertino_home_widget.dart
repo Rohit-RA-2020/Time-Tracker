b5031132d2ef6ddb9a89767e1869f5ca65be3ca5
ROHIT-RA-2020 <ranjanrohit812@gmail.com>
2021-05-12 22:35:52 +0530

    Created list of tabItems

--- lib/app/home/cupertino_home_widget.dart
+++ lib/app/home/cupertino_home_widget.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter/app/home/tab_item.dart';
 
 class CupertinoHomeScaffold extends StatelessWidget {
-  const CupertinoHomeScaffold({Key key, @ required this.currentTab, @required this.onSelectTab})
+  const CupertinoHomeScaffold(
+      {Key key, @required this.currentTab, @required this.onSelectTab})
       : super(key: key);
 
   final TabItem currentTab;
@@ -10,6 +12,18 @@ class CupertinoHomeScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [],
+        onTap: (index) => onSelectTab(TabItem.values[index]),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem tabItem) {
+    return BottomNavigationBarItem(
+      icon: Icon(),
+      label: '',
+    );
   }
 }
--- lib/app/home/tab_item.dart
+++ lib/app/home/tab_item.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/material.dart';
+
 enum TabItem { job, entries, account }
 
+class TabItemData {
+  const TabItemData({@required this.title, @required this.icon});
+  final String title;
+  final IconData icon;
 
+  static const Map<TabItem, TabItemData> allTabs = {
+    TabItem.job: TabItemData(title: 'Jobs', icon: Icons.work),
+    TabItem.entries: TabItemData(title: 'Entries', icon: Icons.view_headline),
+    TabItem.account: TabItemData(title: 'Account', icon: Icons.work),
+  };
+}
